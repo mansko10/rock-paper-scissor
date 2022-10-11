e037dcaec8c05984f914b55de751799ad5ee6b78
mansko10 <amannan2132@gmail.com>
2022-10-11 19:45:33 +0600

    Add selectors

---
+++ script.js
@@ -0,0 +1,5 @@
+let choices = document.querySelector('.choices');
+let feedback = document.querySelector('.feedback');
+let computerScore = document.querySelector('.computer');
+let playerScore = document.querySelector('.playerScore');
+let result = document.querySelector('.result');
